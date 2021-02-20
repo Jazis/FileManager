0f3734e1344a72bf7af3170ea9b4eec510d26d57
Jazis <gulko46@gmail.com>
2021-02-21 02:49:54 +0300

    upd

--- FileManager/Form2.Designer.cs
+++ FileManager/Form2.Designer.cs
@@ -166,6 +166,7 @@ namespace FileManager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(396, 502);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
